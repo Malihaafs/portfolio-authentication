91a7cb4183348b82357fce592883ebc046361df8
root <root@DESKTOP-OK408RO.localdomain>
2022-12-02 00:56:50 +0600

    User Edit, Update, Delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,9 +35,38 @@ exports.loginUser = async (req, res) => {
     }    
 };
 
-// Login   => /list [Get]
+// User List    => /list [Get]
 exports.userList = async (req, res) => {
-    const users = await User.find().sort({ username: 1 });
+    const users = await User.find().sort({ contact_name: 1 });
 
-    res.render('users/index', { users });    
+    res.render('users/index', { users });
+};
+
+// User Edit Form   => /edit/<id> [Get]
+exports.userEdit = async(req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    res.render('users/edit', { user });
+};
+
+// User Update      => /update/<id> [Post]
+exports.userUpdate = async(req, res) => {
+    const { id } = req.params;
+    const { username, email, contact_name, contact_number } = req.body;
+
+    await User.findOneAndUpdate({ _id: id }, {
+        username, email, contact_name, contact_number
+    }, { upsert: true, useFindAndModify: false });
+
+    res.redirect('/user');
+};
+
+// User Delete      => /delete/<id> [Get]
+exports.userDelete = async(req, res) => {
+    const { id } = req.params;
+
+    await User.findByIdAndDelete({ _id: id });
+    
+    res.redirect('/user');    
 };
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { loginForm, loginUser, userList } = require('../controllers/userController');
+const { loginForm, loginUser, userList, userEdit, userUpdate, userDelete } = require('../controllers/userController');
 const { isAuthenticated, isGuest } = require('../middlewares/auth');
 
 router.route('/login')
             .get(isGuest, loginForm)
             .post(isGuest, loginUser);
 
-router.route('/')
-            .get(isAuthenticated, userList);
+router.route('/').get(isAuthenticated, userList);
+router.route('/edit/:id').get(isAuthenticated, userEdit);
+router.route('/update/:id').post(isAuthenticated, userUpdate);
+router.route('/delete/:id').get(isAuthenticated, userDelete);
 
 module.exports = router;
